36370c4a48e0e0ee85f842beb317fcc015e59c90
Noolayko <rawahankwanga148@gmail.com>
2024-08-09 11:10:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -63,7 +63,7 @@ module.exports = {
   HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
   aitts_Voice_Id: process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
-  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "privat",
+  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "Privat",
   LANG: (process.env.THEME || "F I").toUpperCase(),
 };
 global.rank = "updated";
